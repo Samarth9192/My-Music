ea8298e0c27d73e7cf0e8971276a98af65eee593
Samarth9192 <samarthsneh7061@gmail.com>
2025-02-11 22:52:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,16 +38,17 @@ function seekup(mouseX) {
 async function getSongs() {
     let a = await fetch("https://samarth9192.github.io/My-Music/songs.json");
     let response = await a.json();
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a");
-    let songs = [];
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href);
-        }
-    }
+    // let div = document.createElement("div");
+    // div.innerHTML = response.songs;
+    // let as = div.getElementsByTagName("a");
+    // let songs = [];
+    // for (let index = 0; index < as.length; index++) {
+    //     const element = as[index];
+    //     if (element.href.endsWith(".mp3")) {
+    //         songs.push(element.href);
+    //     }
+    // }
+    const songs = response.songs.map(song => "https://samarth9192.github.io/My-Music/"+song);
     return songs;
 }
 function playMusic(title, pause = false) {
